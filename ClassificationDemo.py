da198c0221fdd3426fc1a9247664ccd461b6a8df
Luxit Kapoor <luxitkapoor@Luxits-MacBook-Pro.local>
2018-02-21 12:41:58 +0530

    Added Comments, Formated

--- ClassificationDemo.py
+++ ClassificationDemo.py
@@ -1,27 +1,31 @@
 import sklearn
 
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+#Load data set
 data = load_breast_cancer()
 
+#Seperate dictionary keys to different sets
 labelNames = data['target_names']
 labels = data['target']
 featureNames = data['feature_names']
 features = data['data']
 
-from sklearn.model_selection import train_test_split
-
+#Split the data set into training and testing sets
 train, test, trainLabels, testLabels = train_test_split(features, labels, test_size = 0.33, random_state = 42)
 
-from sklearn.naive_bayes import GaussianNB
-
+#Initialize the Naive Bayes Classifier
 gnb = GaussianNB()
 
+#Train the model
 model = gnb.fit(train, trainLabels)
 
+#Use the testing data to make prediction
 preds = gnb.predict(test)
 print(preds)
 
-from sklearn.metrics import accuracy_score
-
+#Calculate Accuracy
 print(accuracy_score(testLabels, preds))
